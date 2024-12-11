66fee0342634e7820163193166327b6937c8c97a
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2024-12-11 00:23:18 +0000

    Update src/App.tsx [BoltSync]

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { MenuCard } from './components/MenuCard';
-import { Cart } from './components/Cart';
-import { OrderForm } from './components/OrderForm';
 import { CartProvider } from './context/CartContext';
+import { AuthProvider } from './context/AuthContext';
 import { fetchMenu } from './services/api';
 import { MenuItem } from './types';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
-import Home2 from './pages/home2'; // Import Home page
-import Home from './pages/Home'; // Import Home page
-import About from './pages/About'; // Import About page
-import Menu from './pages/Menu'; // Import Menu page
-import Contact from './pages/Contact'; // Import Contact page
+import Home from './pages/Home';
+import Orders from './pages/Orders';
+import Menu from './pages/Menu';
+import Contact from './pages/Contact';
+import CartPage from './pages/Cart';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import LocalMenu from './pages/LocalMenu';
+import ContinentalMenu from './pages/ContinentalMenu';
 
 function App() {
   const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
@@ -51,21 +54,27 @@ function App() {
   }
 
   return (
-    <CartProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-100">
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home menuItems={menuItems} />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/home2" element={<Home2 />} />
-          </Routes>
-        </div>
-        <Footer />
-      </Router>
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <Router>
+          <div className="min-h-screen bg-gray-100">
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home menuItems={menuItems} />} />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/menu/local" element={<LocalMenu />} />
+              <Route path="/menu/continental" element={<ContinentalMenu />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </div>
+          <Footer />
+        </Router>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
