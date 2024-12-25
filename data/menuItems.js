172ff4a6ceac332d43995badaad47379cb8f8d90
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2024-12-25 23:48:33 +0000

    Update data/menuItems.js [BoltSync]

--- data/menuItems.js
+++ data/menuItems.js
@@ -4,7 +4,7 @@ export const menuItems = [
     name: 'Margherita Pizza',
     description: 'Fresh tomatoes, mozzarella cheese, basil, and olive oil',
     price: 14.99,
-    category: 'Pizza',
+    category: 'continental',
     image: 'https://images.unsplash.com/photo-1604068549290-dea0e4a305ca?auto=format&fit=crop&w=800&q=80'
   },
   {
@@ -12,7 +12,7 @@ export const menuItems = [
     name: 'Classic Burger',
     description: 'Angus beef patty with lettuce, tomato, and special sauce',
     price: 12.99,
-    category: 'Burgers',
+    category: 'continental',
     image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=800&q=80'
   },
   {
@@ -20,7 +20,7 @@ export const menuItems = [
     name: 'Caesar Salad',
     description: 'Crisp romaine lettuce, croutons, parmesan, and Caesar dressing',
     price: 9.99,
-    category: 'Salads',
+    category: 'continental',
     image: 'https://images.unsplash.com/photo-1550304943-4f24f54ddde9?auto=format&fit=crop&w=800&q=80'
   },
   {
@@ -28,7 +28,7 @@ export const menuItems = [
     name: 'Miami Salad',
     description: 'Roasted Fresh lettuce, croutons, parmesan, and Caesar dressing',
     price: 13.99,
-    category: 'Salads',
+    category: 'continental',
     image: 'https://images.unsplash.com/photo-1550304943-4f24f54ddde9?auto=format&fit=crop&w=800&q=80'
   },
   {
@@ -36,7 +36,7 @@ export const menuItems = [
     name: 'Jollof Rice with Chicken',
     description: 'tomatoes, onions, peppers, spices',
     price: 12.99,
-    category: 'Local dishes',
+    category: 'local',
     image: 'https://drive.google.com/u/0/drive-viewer/AKGpihYfJJTuKjKoaQll2RaZyEgXKN53c8OvAV8_k9iiYnI5glx_FvRUezsL0fKm9Y1pE6J1D33YDT2oTCeJraAzEuGEGX4MhH772rg=s1600-rw-v1?auto=format&fit=crop&w=800&q=80'
   }
 ];
